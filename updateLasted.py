aea50be03c5cd2539b7d3f95f2b0cb3569ee03e5
Kong-droid <63504831+eunie9498@users.noreply.github.com>
2025-01-21 19:50:18 +0900

    Create updateLasted.py

---
+++ updateLasted.py
@@ -0,0 +1,14 @@
+import feedparser
+uri = "https://kong-droid.com/rss"
+feed = feedparser.parse(uri)
+
+markdown_text = """
+#### Latest Posting Check Here
+"""
+
+for i in feed['entries'][:5]:
+	markdown_text += f"[{i[;title']}]({i['link']}) <br>"
+
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
