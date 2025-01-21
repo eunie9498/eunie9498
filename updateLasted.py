a8345b459e6c52bd432e3319028e98ba428bd523
Kong-droid <63504831+eunie9498@users.noreply.github.com>
2025-01-21 20:07:26 +0900

    Update updateLasted.py

--- updateLasted.py
+++ updateLasted.py
@@ -3,7 +3,20 @@ uri = "https://kong-droid.com/rss"
 feed = feedparser.parse(uri)
 
 markdown_text = """
-#### Latest Posting Check Here
+## ⚒️ 기술스택
+
+Kotlin, Coroutine, Hilt, MVVM, MPAndroidChart, RoomDB, REST API, DataBinding, ViewBinding, Dagger2, CustomLayout, RxJava, Java
+
+## ⚙️ Tools
+
+Android Studio, Figma, Zeplin, GitHub, Git, Swagger, Amplitude, Atom, Slack, Jira, Confluence, Notion,
+
+
+![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=eunie9498&show_icons=true&theme=radical)
+
+
+#### ✅  Latest Posting Check Here 
+
 """
 
 for i in feed['entries'][:5]:
